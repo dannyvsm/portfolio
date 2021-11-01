c6c5cc9e182d967bcd347dd34059fd4a739043ea
dannyvsm <31244585+dannyvsm@users.noreply.github.com>
2021-11-01 17:14:19 +0000

    navbar icons changed and styled

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./footer.css";
 
 function Footer() {
   return (
-    <div>
-      <p>footer</p>
+    <div className="footer">
+      <div className="f-links">
+        <p>hello</p>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
     </div>
   );
 }
--- src/components/header/navbar/Navbar.js
+++ src/components/header/navbar/Navbar.js
@@ -1,27 +1,39 @@
 import React from "react";
 import "./navbar.css";
-import FolioLogo from "../../imgs/dvlogo.png";
+import { BiCool } from "react-icons/bi";
+import { BiCodeBlock } from "react-icons/bi";
+import { BiMessageDots } from "react-icons/bi";
 
 function Navbar() {
   return (
     <div className="navbar">
       <div className="logo">
-        <image src={FolioLogo} alt="logo image" className="logo-img" />
+        <a href="/">
+          <h2 className="logo-text">danny vincent</h2>
+        </a>
       </div>
-      <nav className="links">
-        <a href="/" className="navlink">
-          home
-        </a>
-        <a href="/about" className="navlink">
-          about
-        </a>
-        <a href="/projects" className="navlink">
-          projects
-        </a>
-        <a href="/contacts" className="navlink">
-          contact
-        </a>
-      </nav>
+      <div className="menu">
+        <div className="web-menu">
+          <nav className="weblinks">
+            <a href="/about" className="navlink">
+              <BiCool
+                color="#272b27"
+                variant="bold"
+                size={24}
+                className="nav-icon"
+              />
+            </a>
+            <a href="/projects" className="navlink">
+              <BiCodeBlock color="#272b27" variant="bold" size={24} />
+            </a>
+            <a href="/contacts" className="navlink">
+              <BiMessageDots color="#272b27" variant="bold" size={24} />
+            </a>
+          </nav>
+        </div>
+
+        <div className="mobile-menu"></div>
+      </div>
     </div>
   );
 }
